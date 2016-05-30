4485271d56f8e1d55c4b2d47c6a0ced0fdc70791
Dave Shifflett <dave@corpulentcoffee.com>
2016-05-30 09:16:38 -0500

    Restore Google service from AwesomeTTS v1.5.0

--- addon/awesometts/service/__init__.py
+++ addon/awesometts/service/__init__.py
@@ -32,7 +32,7 @@ from .ekho import Ekho
 from .espeak import ESpeak
 from .festival import Festival
 from .fluencynl import FluencyNl
-# from .google import Google
+from .google import Google
 from .howjsay import Howjsay
 from .imtranslator import ImTranslator
 from .ispeech import ISpeech
@@ -63,7 +63,7 @@ __all__ = [
     'ESpeak',
     'Festival',
     'FluencyNl',
-    # 'Google',
+    'Google',
     'Howjsay',
     'ImTranslator',
     'ISpeech',
